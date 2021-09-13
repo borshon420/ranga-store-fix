55c3197badb9a9917534e4481742bd3a3e7d3800
borshon420 <borshonarafat420@gmail.com>
2021-09-13 21:32:46 +0600

    style added

--- js/app.js
+++ js/app.js
@@ -20,8 +20,8 @@ const showProducts = (products) => {
       <h3>${product.title}</h3>
       <p>Category: ${product.category}</p>
       <h2>Price: $ ${product.price}</h2>
-      <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-success">add to cart</button>
-      <button id="details-btn" class="btn btn-danger">Details</button>
+      <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now">add to cart</button>
+      <button id="details-btn" class="btn-details">Details</button>
         <div>
             <p class="rating-count">People rated: ${product.rating.count}</p>
             <i class="fas fa-star filled"> ${product.rating.rate}</i>
